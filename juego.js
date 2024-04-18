74521d5e6fc207b49e338138e00a6ce280d9b626
1Yubran1 <paulyubranp@gmail.com>
2024-04-18 12:45:27 -0600

    avance 1.1.1.1

--- juego.js
+++ juego.js
@@ -15,6 +15,7 @@ for (var i = 0; i < piezas.length; i++) {
     seleccionarElemento(evt);
   });
 }
+
 /*
 var piezas = document.getElementsByClassName('movil');
 var tamWidth  = [100, 40, 10, 150, 100, 50, 100, 200, 60, 50, 100, 100, 110, 160, 100, 120, 30, 300, 200, 100, 100, 52];
